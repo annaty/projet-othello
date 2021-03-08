0a0155125a0a78c9a9572822bf4bd8efa190df2c
annaty <tylkowskanna@gmail.com>
2021-03-08 10:44:06 +0100

    la construction d'une grille

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-#python
-#
-#Matis ici c'est matis
-#Hello
-#totop YAYYYYYYY
-# Commentaire On est super bon
-# Salut les plus beaux 
-print("hello everybody")
-print("Matireiguhrzfusdfoiuzs")
-print("le sang")
-print("Aie aie les bg")
+
+for row in range(0,9):
+    if row == 0:
+        print("  ", end="")
+        for index in range(1,9):                
+                print(index, end=" ")
+        print()
+    else :
+        print(row, end=" ")
+        for dot in range(0,8):
+            print(".", end=" ")
+        print()
