40c43583468208b80821c678532347f1c11a1d78
annaty <tylkowskanna@gmail.com>
2021-03-08 10:43:43 +0100

    la construction d'une grille

--- main.py
+++ main.py
@@ -2,6 +2,10 @@
 #
 #Matis ici c'est matis
 #Hello
-totop YAYYYYYYY
+#totop YAYYYYYYY
 # Commentaire On est super bon
-# Salut les plus beaux
+# Salut les plus beaux 
+print("hello everybody")
+print("Matireiguhrzfusdfoiuzs")
+print("le sang")
+print("Aie aie les bg")
