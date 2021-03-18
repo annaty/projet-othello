6d865b4470cba03e5b87f83d9332c997deb0d0d8
antonin187 <antonin.simon@epsi.fr>
2021-03-18 01:14:10 +0100

    'comment'

--- functions.py
+++ functions.py
@@ -1,6 +1,7 @@
 #functions
 import os
 
+# Fonction qui affiche la grille
 def printGrille(grille_par):
     for element in grille_par:
         for truc in element:
@@ -11,7 +12,7 @@ def printGrille(grille_par):
 def cls():
     os.system('cls' if os.name == 'nt' else 'clear')
 
-
+# Fonction qui vérifié ce que l'on a autour du pion qu'on joue
 def check_autour(grille, row, column):
     if (grille[row - 1][column] != "x" and grille[row + 1][column] != "x" and grille[row][column - 1] != "x" 
             and grille[row][column + 1] != "x" and grille[row + 1][column - 1] != "x" and grille[row + 1][column + 1] != "x" 
@@ -22,6 +23,7 @@ def check_autour(grille, row, column):
     else:
         return True
 
+# Fonction qui retourne les pions "o" à l'horizontale
 def return_o_horizontal(grille, row_j1, column_j1):
     for element in range(len(grille[row_j1])):
         if grille[row_j1][element] == "o" and element != column_j1:
@@ -33,6 +35,7 @@ def return_o_horizontal(grille, row_j1, column_j1):
                     grille[row_j1][index_transform] = "o"
             break
 
+# Fonction qui retourne les pions "x" à l'horizontale
 def return_x_horizontal(grille, row_j2, column_j2):
     for element in range(len(grille[row_j2])):
         if grille[row_j2][element] == "x" and element != column_j2:
@@ -44,6 +47,7 @@ def return_x_horizontal(grille, row_j2, column_j2):
                     grille[row_j2][index_transform] = "x"
             break
 
+# Fonction qui retourne les pions "o" à la verticale
 def return_o_vertical(grille, row_j1, column_j1):
     for row_index in range(len(grille)):
         if grille[row_index][column_j1] == "o" and row_index != row_j1:
@@ -55,6 +59,7 @@ def return_o_vertical(grille, row_j1, column_j1):
                         grille[index_transform][column_j1] = "o"
                 break
 
+# Fonction qui retourne les pions "x" à la verticale
 def return_x_vertical(grille, row_j2, column_j2):
     for row_index in range(len(grille)):
         if grille[row_index][column_j2] == "x" and row_index != row_j2:
@@ -68,6 +73,7 @@ def return_x_vertical(grille, row_j2, column_j2):
 
 #def return_x_diagonal(grille, row_j2, column_j2):
 
+# Fonction qui retourne les pions "o" en diagonale
 def return_o_diagonal(grille, row_j1, column_j1):
     for i in range(1, 2):
         if grille[row_j1 + i][column_j1 + i] == "o": # SE
