3804998427388743e9cc5fb001ad968fcd52428f
annaty <tylkowskanna@gmail.com>
2021-03-26 14:50:18 +0100

    fixed bugs when putting down a piece, optimized starting position code, ameliorated the graphical ui Co-authored-by: MatisGalvin <MatisGalvin@users.noreply.github.com>Co-authored-by: antonin187 <antonin187@users.noreply.github.com># Please enter the commit message for your changes. Lines starting# with '#' will be ignored, and an empty message aborts the commit.## Date:      Fri Mar 26 14:50:18 2021 +0100## On branch master# Your branch is ahead of 'origin/master' by 2 commits.#   (use "git push" to publish your local commits)## Changes to be committed:#	new file:   .gitignore/__pycache__/damier.cpython-38.pyc#	renamed:    __pycache__/functions.cpython-38.pyc -> .gitignore/__pycache__/functions.cpython-38.pyc#	renamed:    __pycache__/tkinter.cpython-38.pyc -> .gitignore/__pycache__/tkinter.cpython-38.pyc#	deleted:    __pycache__/damier.cpython-38.pyc#	modified:   damier.py#	new file:   icon.png#	modified:   main.py#	modified:   test.py#

--- damier.py
+++ damier.py
@@ -2,12 +2,14 @@ from math import *
 # o --> bleu
 # x --> rouge
 from tkinter import *
+from tkinter import ttk
+from tkinter.ttk import *
 
 class Damier():
     def __init__(self, canevas):
         self.can = canevas
         self.lesCases = []
-        self.counter = 1
+        self.move_counter = 1
 
     def creation_grille(self, taille):
         grille = []
@@ -17,38 +19,47 @@ class Damier():
             grille.append([])
             x1 = 10
             x2 = x1 + 30
-            if row == 0:
+            if row == 0: #premier ligne
                 grille[row].append(" ")
-                self.can.create_rectangle(x1, y1, x2, y2, fill = "green")
+                # self.can.create_rectangle(x1, y1, x2, y2, outline='gray64') # top left corner
                 self.lesCases.append([(x1, x2),(y1, y2), False, ""])
                 for column in range(1, taille + 2):
                     if column == taille + 1:
-                        grille[row].append("_")
+                        grille[row].append('_')
+                        self.lesCases.append([(x1, x2),(y1, y2), False, ""])
                     else:
                         x1 += 30
                         x2 += 30
                         grille[row].append(column)
-                        self.can.create_rectangle(x1, y1, x2, y2, fill = "white")
-                        self.can.create_text(x1+15, y1+15, text = column, fill="green")
+                        self.can.create_rectangle(x1, y1, x2, y2, outline='light steel blue')
+                        self.can.create_text(x1+15, y1+15, text = column)
                         self.lesCases.append([(x1, x2),(y1, y2), False, ""])
-                        
-            
-            elif row == taille + 1:
+            elif row == taille + 1: # derniere ligne
+                grille[row].append(" ")
+                self.lesCases.append([(x1, x2),(y1, y2), False, ""])
                 for column in range(1, taille + 2):
-                    grille[row].append("_")
-            else:
+                    if column == taille + 1: # derniere colonne
+                        grille[row].append('_')
+                        self.lesCases.append([(x1, x2),(y1, y2), False, ""])
+                    else:
+                        x1 += 30
+                        x2 += 30
+                        grille[row].append(column)
+                        self.lesCases.append([(x1, x2),(y1, y2), False, ""])
+            else: # autres lignes
                 grille[row].append(row)
-                self.can.create_rectangle(x1, y1, x2, y2, fill = "white")
-                self.can.create_text(x1 +15, y1 + 15, text = grille[row][0], fill="green")
+                self.can.create_rectangle(x1, y1, x2, y2, outline='light steel blue')
+                self.can.create_text(x1 +15, y1 + 15, text = grille[row][0])
                 self.lesCases.append([(x1, x2),(y1, y2), False, ""])
                 x1 += 30
                 x2 += 30
                 for column in range(1, taille + 2):
                     if column == taille + 1:
+                        self.lesCases.append([(x1, x2),(y1, y2), False, ""])
                         grille[row].append("_")
                     else:
                         grille[row].append(".")
-                        self.can.create_rectangle(x1, y1, x2, y2, fill = "white")
+                        self.can.create_rectangle(x1, y1, x2, y2, outline='gray50')
                         self.lesCases.append([(x1, x2),(y1, y2), False, ""])
                         x1 += 30
                         x2 += 30
@@ -57,23 +68,26 @@ class Damier():
             y2 += 30
             
         #=============   position de depart   =======================
-        milieu_ligne = int(floor(len(grille)*30-10) / 2)
-        milieu_colonne = int(floor(len(grille[0])*30-10) / 2)
-        self.can.create_oval(milieu_ligne +2 + 15, milieu_colonne + 2 + 15, milieu_ligne + 28 + 15 , milieu_colonne + 28 + 15, fill='blue')
-        self.lesCases.remove([(milieu_ligne + 15, milieu_ligne + 30 + 15),(milieu_colonne + 15, milieu_colonne + 30 + 15), False, ""])
-        self.lesCases.append([(milieu_ligne + 15, milieu_ligne + 30 + 15),(milieu_colonne + 15, milieu_colonne + 30 + 15), True, "blue"])
+        milieu = int(floor(len(grille)*30-10) / 2)
+        padd = 2
+        
+        self.can.create_oval(milieu + padd + 15, milieu + padd + 15, milieu + 28 + 15 , milieu + 28 + 15, fill='snow', outline='snow')
+        self.lesCases.remove([(milieu + 15, milieu + 30 + 15),(milieu + 15, milieu + 30 + 15), False, ""])
+        self.lesCases.append([(milieu + 15, milieu + 30 + 15),(milieu + 15, milieu + 30 + 15), True, "blue"])
 
-        self.can.create_oval(milieu_ligne +2 - 15, milieu_colonne + 2 - 15, milieu_ligne + 28 - 15 , milieu_colonne + 28 - 15, fill='blue')
-        self.lesCases.remove([(milieu_ligne - 15, milieu_ligne + 30 - 15),(milieu_colonne - 15, milieu_colonne + 30 - 15), False, ""])
-        self.lesCases.append([(milieu_ligne - 15, milieu_ligne + 30 - 15),(milieu_colonne - 15, milieu_colonne + 30 - 15), True, "blue"])
+        self.can.create_oval(milieu + padd - 15, milieu + padd - 15, milieu + 28 - 15 , milieu + 28 - 15, fill='snow', outline='snow')
+        self.lesCases.remove([(milieu - 15, milieu + 30 - 15),(milieu - 15, milieu + 30 - 15), False, ""])
+        self.lesCases.append([(milieu - 15, milieu + 30 - 15),(milieu - 15, milieu + 30 - 15), True, "blue"])
 
-        self.can.create_oval(milieu_ligne +2 + 15,  milieu_colonne + 2 - 15, milieu_ligne + 28 + 15 , milieu_colonne + 28 - 15, fill='red')
-        self.lesCases.remove([(milieu_ligne + 15, milieu_ligne + 30 + 15),(milieu_colonne - 15, milieu_colonne + 30 - 15), False, ""])
-        self.lesCases.append([(milieu_ligne + 15, milieu_ligne + 30 + 15),(milieu_colonne - 15, milieu_colonne + 30 - 15), True, "red"])
+        self.can.create_oval(milieu + padd + 15,  milieu + padd - 15, milieu + 28 + 15 , milieu + 28 - 15, fill='gray24', outline='gray24')
+        self.lesCases.remove([(milieu + 15, milieu + 30 + 15),(milieu - 15, milieu + 30 - 15), False, ""])
+        self.lesCases.append([(milieu + 15, milieu + 30 + 15),(milieu - 15, milieu + 30 - 15), True, "red"])
 
-        self.can.create_oval(milieu_ligne +2 - 15, milieu_colonne + 2 + 15, milieu_ligne + 28 - 15 , milieu_colonne + 28 + 15, fill='red')
-        self.lesCases.remove([(milieu_ligne - 15, milieu_ligne + 30 - 15),(milieu_colonne + 15, milieu_colonne + 30 + 15), False, ""])
-        self.lesCases.append([(milieu_ligne - 15, milieu_ligne + 30 - 15),(milieu_colonne + 15, milieu_colonne + 30 + 15), True, "red"])
+        self.can.create_oval(milieu + padd - 15, milieu + padd + 15, milieu + 28 - 15 , milieu + 28 + 15, fill='gray24', outline='gray24')
+        self.lesCases.remove([(milieu - 15, milieu + 30 - 15),(milieu + 15, milieu + 30 + 15), False, ""])
+        self.lesCases.append([(milieu - 15, milieu + 30 - 15),(milieu + 15, milieu + 30 + 15), True, "red"])
+
+
 
     def check_autour(self, case_list, x1, x2, y1, y2):
         case_counter = 0
@@ -110,34 +124,35 @@ class Damier():
         x = event.x
         y = event.y
 
-        if self.counter %2 == 0:
-            couleur = 'blue'
-        elif self.counter %2 == 1:
-            couleur = 'red'
+        if self.move_counter % 2 == 0:
+            couleur = 'gray24'
+        elif self.move_counter % 2 == 1:
+            couleur = 'snow'
 
         for case in self.lesCases:
             if ((case[0][0] < x < case[0][1]) and (case[1][0] < y < case[1][1])) and (case[2] == False):
                 if self.check_autour(self.lesCases, case[0][0], case[0][1], case[1][0], case[1][1]) == True:
-                    self.can.create_oval(case[0][0] + 2,case[1][0] + 2,case[0][1] - 2,case[1][1] - 2,fill=couleur)
+                    self.can.create_oval(case[0][0] + 2,case[1][0] + 2,case[0][1] - 2,case[1][1] - 2,fill=couleur, outline=couleur)
                     case_index = self.lesCases.index(case)
                     self.lesCases[case_index][2] = True
-                    self.counter += 1
+                    self.move_counter += 1
                 else:
                     error = Toplevel()  # Popup -> Toplevel()
                     error.title('Infos')
                     canerror = Canvas(error, bg='white', height=200, width=200)
                     canerror.pack(side=LEFT)
                     Button(error, text='Quitter', command=error.destroy).pack(padx=10, pady=10)
-                    mytext = canerror.create_text(100, 100, text="Cette case est deja prise")
+                    mytext = canerror.create_text(100, 100, text="Il n'y a rien autour, veuillez choisir un autre champs")
                     mytext.pack()
             elif ((case[0][0] < x < case[0][1]) and (case[1][0] < y < case[1][1])) and (case[2] == True):
                 error = Toplevel()  # Popup -> Toplevel()
                 error.title('Infos')
-                canerror = Canvas(error, bg='white', height=100, width=100)
+                canerror = Canvas(error, bg='white', height=200, width=200)
                 canerror.pack(side=LEFT)
                 Button(error, text='Quitter', command=error.destroy).pack(padx=10, pady=10)
-                mytext = canerror.create_text(100, 100, text="Vous ne pouvez pas placer un pion ici, il doit y avoir au moins 1 pion sur une case adjacente.")
+                mytext = canerror.create_text(100, 100, text="Ce champs est deja pris")
                 mytext.pack()
+
 
     def clear(self):
         "Nettoyage du canevas"
